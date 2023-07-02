c3405f88c566d2a694c66de5e9a526371733b286
nkkn-prog <nkkn.prog@gmail.com>
2023-07-02 23:53:40 +0900

    changeable variable

--- variables.php
+++ variables.php
@@ -5,8 +5,8 @@ $whatever = &$odd;
 
 $whatever = "I like $whatever\n";
 
-echo $whatever;
-echo $odd;
+// echo $whatever;
+// echo $odd;
 
 /*これの出力結果は
 I like 1I like 1 となり、$oddの内容も変更された。
@@ -35,7 +35,7 @@ function ageCount(){
   global $name; //グローバルスコープの関数を用いる時は、ローカルスコープ内で定義してあげる
 
   $age = 13;
-  echo "$name is $age years old";
+  // echo "$name is $age years old";
 }
 
 //static変数について
@@ -45,13 +45,24 @@ function countUp(){
   static $count = 0;
 
     $count++;
-    echo $count;
+    // echo $count;
     if ($count < 10) {
         countUp();
     }
     $count--;
 
 }
-countUp();
+
+//可変変数: 変数名を動的にセットして使用できる。
+//これは一般的なもの。aという変数に、文字型のhelloという文字が入っている。
+$a = 'hello';
+
+//$aの前に$を1つ追加すると、元々aに入っていたhelloという文字列が変数名に変わる。
+//つまり、下の$$aというのは、$helloと同義である。
+$$a = 'world';
+
+// hello worldと出力されるはず
+// echo "$a {$$a}\n";
+// echo "$$a $hello";
 
 ?>
